e69d294764864dae5fe17d37a6e2b06250379f41
mauro-benitez <mauro.cbenitez@gmail.com>
2024-09-29 22:28:26 -0300

    Criando e associando as primeiras Entidades

---
+++ Core/Entity/Autor.cs
@@ -0,0 +1,15 @@
+﻿using Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entidades
+{
+    public class Autor : BaseEntity
+    {
+
+        List<Livro> livros;
+    }
+}
