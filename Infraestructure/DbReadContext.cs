26ee019e146fae34e2a40b0e62ecb1914e948b77
mauro-benitez <mauro.cbenitez@gmail.com>
2024-10-01 23:29:59 -0300

    Criação das Classes de Context e cofigurando a string de conexão

--- Infraestructure/DbReadContext.cs
+++ Infraestructure/DbReadContext.cs
@@ -7,6 +7,8 @@ namespace Infraestructure
 
 
 
+        //context
+
 
 
 
--- Livraria/Program.cs
+++ Livraria/Program.cs
@@ -1,3 +1,7 @@
+using Infraestructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+//db
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+//Read
+var stringConection = configuration.GetValue<string>("ConnectionStringsDbRead");
+builder.Services.AddDbContext<DbReadContext>(options => options.UseSqlServer(stringConection));
+
+
+//Edit
+var stringConectionEdit = configuration.GetValue<string>("ConnectionStringsDbEdit");
+builder.Services.AddDbContext<DbReadContext>(options => options.UseSqlServer(stringConection));
+
+
+
+
 
 var app = builder.Build();
 
